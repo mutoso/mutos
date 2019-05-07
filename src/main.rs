7472538af116e3373974948880164b306cf8792b
Alastair Feille <me@alastair.se>
2019-05-07 15:53:22 -0500

    Add skeleton for a freestanding Rust binary

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,20 @@
     License, v. 2.0. If a copy of the MPL was not distributed with this
     file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
+#![feature(lang_items)]
+#![no_std]
+#![no_main]
 
-fn main()
+use core::panic::PanicInfo;
+
+#[no_mangle]
+fn _start() -> !
 {
+    loop {}
+}
 
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> !
+{
+    loop {}
 }
