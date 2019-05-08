6dcffc60909e807d197986bbc510bc0d44c86b5b
Alastair Feille <me@alastair.se>
2019-05-08 01:55:17 -0500

    Change build to produce a bootable image

--- README.md
+++ README.md
@@ -8,3 +8,9 @@
 [![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
 
 An attempt at a microkernel written in Rust
+
+# Building
+(Requires cargo-xbuild, bootimage, and hjson)
+```
+./build.sh
+```
---
+++ build.sh
@@ -0,0 +1,6 @@
+# Copyright © 2019 Alastair Feille
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+hjson -j x86_64-mutos.hjson > x86_64-mutos.json
+cargo bootimage
